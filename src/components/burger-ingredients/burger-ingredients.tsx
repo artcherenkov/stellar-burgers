e116e4c688708cb9fd61736b17921d8107d0f63a
Артем Черенков <art.cherenkov@gmail.com>
2021-09-17 18:55:55 +0700

    sprint-1/step-1 refactor: добавить вывод моков

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,22 +1,46 @@
 import { useState } from "react";
 import cn from "classnames";
 import {
-  Counter,
   CurrencyIcon,
   Tab,
 } from "@ya.praktikum/react-developer-burger-ui-components";
+import { MOCKS } from "../../utils/data";
 
 import styles from "./burger-ingredients.module.css";
 
 const Type = {
-  BUN: "BUN",
-  SAUCE: "SAUCE",
-  FILLING: "FILLING",
+  BUN: "bun",
+  SAUCE: "sauce",
+  MAIN: "main",
+};
+
+type TIngredient = {
+  _id: string;
+  name: string;
+  type: string;
+  proteins: number;
+  fat: number;
+  carbohydrates: number;
+  calories: number;
+  price: number;
+  image: string;
+  image_mobile: string;
+  image_large: string;
+  __v: number;
+};
+
+const sortIngredientsByType = (data: TIngredient[]) => {
+  const buns = data.filter((i) => i.type === Type.BUN);
+  const mains = data.filter((i) => i.type === Type.MAIN);
+  const sauces = data.filter((i) => i.type === Type.SAUCE);
+  return { buns, mains, sauces };
 };
 
 const BurgerIngredients = () => {
   const [current, setCurrent] = useState(Type.BUN);
   const onTabClick = (value: string) => setCurrent(value);
+
+  const { buns, mains, sauces } = sortIngredientsByType(MOCKS);
 
   return (
     <section className={styles.root}>
@@ -39,8 +63,8 @@ const BurgerIngredients = () => {
           Соусы
         </Tab>
         <Tab
-          value={Type.FILLING}
-          active={current === Type.FILLING}
+          value={Type.MAIN}
+          active={current === Type.MAIN}
           onClick={onTabClick}
         >
           Начинки
@@ -49,265 +73,87 @@ const BurgerIngredients = () => {
       <div className={cn(styles.ingredientsContainer, "custom-scroll")}>
         <h2 className="text text_type_main-medium mb-6">Булки</h2>
         <ul className={styles.ingredientsList}>
-          <li className={styles.ingredientsItem}>
-            <div className={styles.burgerIngredient}>
-              <Counter count={1} size="default" />
-              <img
-                className="pr-4 pl-4 mb-1"
-                src="https://code.s3.yandex.net/react/code/bun-02.png"
-                alt="Краторная булка N-200i"
-              />
-              <p className={styles.ingredientCost}>
-                <span className="text text_type_digits-default mr-2">20</span>{" "}
-                <CurrencyIcon type="primary" />
-              </p>
-              <h3
-                className={cn(
-                  styles.ingredientTitle,
-                  "text text_type_main-default"
-                )}
-              >
-                Краторная булка N-200i булка N-200i
-              </h3>
-            </div>
-          </li>
-          <li className={styles.ingredientsItem}>
-            <div className={styles.burgerIngredient}>
-              <img
-                className="pr-4 pl-4 mb-1"
-                src="https://code.s3.yandex.net/react/code/bun-02.png"
-                alt="Краторная булка N-200i"
-              />
-              <p className={styles.ingredientCost}>
-                <span className="text text_type_digits-default mr-2">20</span>{" "}
-                <CurrencyIcon type="primary" />
-              </p>
-              <h3
-                className={cn(
-                  styles.ingredientTitle,
-                  "text text_type_main-default"
-                )}
-              >
-                Краторная булка N-200i булка N-200i
-              </h3>
-            </div>
-          </li>
-          <li className={styles.ingredientsItem}>
-            <div className={styles.burgerIngredient}>
-              <img
-                className="pr-4 pl-4 mb-1"
-                src="https://code.s3.yandex.net/react/code/bun-02.png"
-                alt="Краторная булка N-200i"
-              />
-              <p className={styles.ingredientCost}>
-                <span className="text text_type_digits-default mr-2">20</span>{" "}
-                <CurrencyIcon type="primary" />
-              </p>
-              <h3
-                className={cn(
-                  styles.ingredientTitle,
-                  "text text_type_main-default"
-                )}
-              >
-                Краторная булка N-200i булка N-200i
-              </h3>
-            </div>
-          </li>
-          <li className={styles.ingredientsItem}>
-            <div className={styles.burgerIngredient}>
-              <img
-                className="pr-4 pl-4 mb-1"
-                src="https://code.s3.yandex.net/react/code/bun-02.png"
-                alt="Краторная булка N-200i"
-              />
-              <p className={styles.ingredientCost}>
-                <span className="text text_type_digits-default mr-2">20</span>{" "}
-                <CurrencyIcon type="primary" />
-              </p>
-              <h3
-                className={cn(
-                  styles.ingredientTitle,
-                  "text text_type_main-default"
-                )}
-              >
-                Краторная булка N-200i булка N-200i
-              </h3>
-            </div>
-          </li>
+          {buns.map((item) => (
+            <li className={styles.ingredientsItem}>
+              <div className={styles.burgerIngredient}>
+                <img
+                  className="pr-4 pl-4 mb-1"
+                  src={item.image}
+                  alt={item.name}
+                />
+                <p className={styles.ingredientCost}>
+                  <span className="text text_type_digits-default mr-2">
+                    {item.price}
+                  </span>{" "}
+                  <CurrencyIcon type="primary" />
+                </p>
+                <h3
+                  className={cn(
+                    styles.ingredientTitle,
+                    "text text_type_main-default"
+                  )}
+                >
+                  {item.name}
+                </h3>
+              </div>
+            </li>
+          ))}
         </ul>
         <h2 className="text text_type_main-medium mb-6">Соусы</h2>
         <ul className={styles.ingredientsList}>
-          <li className={styles.ingredientsItem}>
-            <div className={styles.burgerIngredient}>
-              <img
-                className="pr-4 pl-4 mb-1"
-                src="https://code.s3.yandex.net/react/code/bun-02.png"
-                alt="Краторная булка N-200i"
-              />
-              <p className={styles.ingredientCost}>
-                <span className="text text_type_digits-default mr-2">20</span>{" "}
-                <CurrencyIcon type="primary" />
-              </p>
-              <h3
-                className={cn(
-                  styles.ingredientTitle,
-                  "text text_type_main-default"
-                )}
-              >
-                Краторная булка N-200i булка N-200i
-              </h3>
-            </div>
-          </li>
-          <li className={styles.ingredientsItem}>
-            <div className={styles.burgerIngredient}>
-              <img
-                className="pr-4 pl-4 mb-1"
-                src="https://code.s3.yandex.net/react/code/bun-02.png"
-                alt="Краторная булка N-200i"
-              />
-              <p className={styles.ingredientCost}>
-                <span className="text text_type_digits-default mr-2">20</span>{" "}
-                <CurrencyIcon type="primary" />
-              </p>
-              <h3
-                className={cn(
-                  styles.ingredientTitle,
-                  "text text_type_main-default"
-                )}
-              >
-                Краторная булка N-200i булка N-200i
-              </h3>
-            </div>
-          </li>
-          <li className={styles.ingredientsItem}>
-            <div className={styles.burgerIngredient}>
-              <img
-                className="pr-4 pl-4 mb-1"
-                src="https://code.s3.yandex.net/react/code/bun-02.png"
-                alt="Краторная булка N-200i"
-              />
-              <p className={styles.ingredientCost}>
-                <span className="text text_type_digits-default mr-2">20</span>{" "}
-                <CurrencyIcon type="primary" />
-              </p>
-              <h3
-                className={cn(
-                  styles.ingredientTitle,
-                  "text text_type_main-default"
-                )}
-              >
-                Краторная булка N-200i булка N-200i
-              </h3>
-            </div>
-          </li>
-          <li className={styles.ingredientsItem}>
-            <div className={styles.burgerIngredient}>
-              <img
-                className="pr-4 pl-4 mb-1"
-                src="https://code.s3.yandex.net/react/code/bun-02.png"
-                alt="Краторная булка N-200i"
-              />
-              <p className={styles.ingredientCost}>
-                <span className="text text_type_digits-default mr-2">20</span>{" "}
-                <CurrencyIcon type="primary" />
-              </p>
-              <h3
-                className={cn(
-                  styles.ingredientTitle,
-                  "text text_type_main-default"
-                )}
-              >
-                Краторная булка N-200i булка N-200i
-              </h3>
-            </div>
-          </li>
+          {sauces.map((item) => (
+            <li className={styles.ingredientsItem}>
+              <div className={styles.burgerIngredient}>
+                <img
+                  className="pr-4 pl-4 mb-1"
+                  src={item.image}
+                  alt={item.name}
+                />
+                <p className={styles.ingredientCost}>
+                  <span className="text text_type_digits-default mr-2">
+                    {item.price}
+                  </span>{" "}
+                  <CurrencyIcon type="primary" />
+                </p>
+                <h3
+                  className={cn(
+                    styles.ingredientTitle,
+                    "text text_type_main-default"
+                  )}
+                >
+                  {item.name}
+                </h3>
+              </div>
+            </li>
+          ))}
         </ul>
         <h2 className="text text_type_main-medium mb-6">Начинки</h2>
         <ul className={styles.ingredientsList}>
-          <li className={styles.ingredientsItem}>
-            <div className={styles.burgerIngredient}>
-              <img
-                className="pr-4 pl-4 mb-1"
-                src="https://code.s3.yandex.net/react/code/bun-02.png"
-                alt="Краторная булка N-200i"
-              />
-              <p className={styles.ingredientCost}>
-                <span className="text text_type_digits-default mr-2">20</span>{" "}
-                <CurrencyIcon type="primary" />
-              </p>
-              <h3
-                className={cn(
-                  styles.ingredientTitle,
-                  "text text_type_main-default"
-                )}
-              >
-                Краторная булка N-200i булка N-200i
-              </h3>
-            </div>
-          </li>
-          <li className={styles.ingredientsItem}>
-            <div className={styles.burgerIngredient}>
-              <img
-                className="pr-4 pl-4 mb-1"
-                src="https://code.s3.yandex.net/react/code/bun-02.png"
-                alt="Краторная булка N-200i"
-              />
-              <p className={styles.ingredientCost}>
-                <span className="text text_type_digits-default mr-2">20</span>{" "}
-                <CurrencyIcon type="primary" />
-              </p>
-              <h3
-                className={cn(
-                  styles.ingredientTitle,
-                  "text text_type_main-default"
-                )}
-              >
-                Краторная булка N-200i булка N-200i
-              </h3>
-            </div>
-          </li>
-          <li className={styles.ingredientsItem}>
-            <div className={styles.burgerIngredient}>
-              <img
-                className="pr-4 pl-4 mb-1"
-                src="https://code.s3.yandex.net/react/code/bun-02.png"
-                alt="Краторная булка N-200i"
-              />
-              <p className={styles.ingredientCost}>
-                <span className="text text_type_digits-default mr-2">20</span>{" "}
-                <CurrencyIcon type="primary" />
-              </p>
-              <h3
-                className={cn(
-                  styles.ingredientTitle,
-                  "text text_type_main-default"
-                )}
-              >
-                Краторная булка N-200i булка N-200i
-              </h3>
-            </div>
-          </li>
-          <li className={styles.ingredientsItem}>
-            <div className={styles.burgerIngredient}>
-              <img
-                className="pr-4 pl-4 mb-1"
-                src="https://code.s3.yandex.net/react/code/bun-02.png"
-                alt="Краторная булка N-200i"
-              />
-              <p className={styles.ingredientCost}>
-                <span className="text text_type_digits-default mr-2">20</span>{" "}
-                <CurrencyIcon type="primary" />
-              </p>
-              <h3
-                className={cn(
-                  styles.ingredientTitle,
-                  "text text_type_main-default"
-                )}
-              >
-                Краторная булка N-200i булка N-200i
-              </h3>
-            </div>
-          </li>
+          {mains.map((item) => (
+            <li className={styles.ingredientsItem}>
+              <div className={styles.burgerIngredient}>
+                <img
+                  className="pr-4 pl-4 mb-1"
+                  src={item.image}
+                  alt={item.name}
+                />
+                <p className={styles.ingredientCost}>
+                  <span className="text text_type_digits-default mr-2">
+                    {item.price}
+                  </span>{" "}
+                  <CurrencyIcon type="primary" />
+                </p>
+                <h3
+                  className={cn(
+                    styles.ingredientTitle,
+                    "text text_type_main-default"
+                  )}
+                >
+                  {item.name}
+                </h3>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </section>
