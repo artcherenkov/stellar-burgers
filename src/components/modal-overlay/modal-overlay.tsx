f1aaba261a65e8ad54f67601f79e631830c3b96d
Артем Черенков <art.cherenkov@gmail.com>
2021-09-20 22:34:32 +0700

    sprint-1/step-2 fix: исправить логику работы Modal:  теперь children рендерится как соседский элемент ModalOverview и не спускается дальше Modal

--- src/components/modal-overlay/modal-overlay.tsx
+++ src/components/modal-overlay/modal-overlay.tsx
@@ -1,27 +1,12 @@
-import React, { SyntheticEvent, useEffect, useRef } from "react";
+import React, { SyntheticEvent, useRef } from "react";
 import styles from "./modal-overlay.module.css";
-import cn from "classnames";
 
 interface IModalOverlay {
-  children: React.ReactNode;
-  open: boolean;
   onClose: () => void;
 }
 
 const ModalOverlay = (props: IModalOverlay) => {
   const overlayRef = useRef<HTMLDivElement>(null);
-
-  const onEscKeydown = (evt: KeyboardEvent) => {
-    if (evt.key === "Escape") {
-      props.onClose();
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("keydown", onEscKeydown);
-
-    return () => document.removeEventListener("keydown", onEscKeydown);
-  }, []);
 
   const handleClick = (evt: SyntheticEvent) => {
     if (evt.target === overlayRef.current) {
@@ -29,15 +14,7 @@ const ModalOverlay = (props: IModalOverlay) => {
     }
   };
 
-  return (
-    <div
-      className={cn(styles.root, { [styles.root_open]: props.open })}
-      onClick={handleClick}
-      ref={overlayRef}
-    >
-      {props.children}
-    </div>
-  );
+  return <div className={styles.root} onClick={handleClick} ref={overlayRef} />;
 };
 
 export default ModalOverlay;
--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
+import cn from "classnames";
+import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+
 import ModalOverlay from "../modal-overlay/modal-overlay";
 import styles from "./modal.module.css";
-import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
 const modalRoot = document.getElementById("react-modals")!;
 
@@ -13,15 +15,30 @@ interface IModal {
 }
 
 const Modal = (props: IModal) => {
+  const onEscKeydown = (evt: KeyboardEvent) => {
+    if (evt.key === "Escape") {
+      props.onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", onEscKeydown);
+
+    return () => document.removeEventListener("keydown", onEscKeydown);
+  }, [onEscKeydown]);
+
   return ReactDOM.createPortal(
-    <ModalOverlay {...props}>
-      <div className={styles.root}>
-        <button className={styles.closeButton} onClick={props.onClose}>
-          <CloseIcon type="primary" />
-        </button>
-        {props.children}
+    <>
+      <div className={cn(styles.root, { [styles.root_open]: props.open })}>
+        <ModalOverlay onClose={props.onClose} />
+        <div className={styles.content}>
+          <button className={styles.closeButton} onClick={props.onClose}>
+            <CloseIcon type="primary" />
+          </button>
+          {props.children}
+        </div>
       </div>
-    </ModalOverlay>,
+    </>,
     modalRoot
   );
 };
