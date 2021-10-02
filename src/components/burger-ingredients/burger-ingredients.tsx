72d72f7d3513680986a7710d49f9586d3109af05
Artem Cherenkov <art.cherenkov@gmail.com>
2021-10-02 21:04:39 +0700

    sprint-2/step-2 feat: настроить автопереключение табов при скролле

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import cn from "classnames";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 
@@ -22,6 +22,42 @@ const BurgerIngredients = () => {
       <Ingredient {...item} />
     </li>
   );
+
+  const [headersScroll, setHeadersScroll] = useState([0, 0, 0]);
+  const [offsetTop, setOffsetTop] = useState(0);
+
+  const tabsRef = useRef<HTMLDivElement>(null);
+  const headersRef = useRef<any>(null);
+
+  useEffect(() => {
+    setOffsetTop(Number(tabsRef.current?.getBoundingClientRect().y));
+    const headerElements = document.querySelectorAll(".ing-type-header");
+    headersRef.current = Array.from(headerElements);
+
+    setHeadersScroll(
+      headersRef.current.map(
+        (elem: HTMLHeadingElement) => elem.getBoundingClientRect().y - offsetTop
+      )
+    );
+  }, [offsetTop]);
+
+  useEffect(() => {
+    if (headersScroll[2] < 0) {
+      setCurrent(IngredientType.MAIN);
+    } else if (headersScroll[1] < 0) {
+      setCurrent(IngredientType.SAUCE);
+    } else if (headersScroll[0] <= 0) {
+      setCurrent(IngredientType.BUN);
+    }
+  }, [headersScroll]);
+
+  const handleScroll = () => {
+    setHeadersScroll(
+      headersRef.current.map(
+        (elem: HTMLHeadingElement) => elem.getBoundingClientRect().y - offsetTop
+      )
+    );
+  };
 
   return (
     <section className={styles.root}>
@@ -47,7 +83,6 @@ const BurgerIngredients = () => {
             Соусы
           </Tab>
         </a>
-
         <a className={styles.tabLink} href={`#${IngredientType.MAIN}`}>
           <Tab
             value={IngredientType.MAIN}
@@ -58,22 +93,34 @@ const BurgerIngredients = () => {
           </Tab>
         </a>
       </div>
-      <div className={cn(styles.ingredientsContainer, "custom-scroll")}>
-        <h2 id={IngredientType.BUN} className="text text_type_main-medium mb-6">
+      <div
+        className={cn(styles.ingredientsContainer, "custom-scroll")}
+        onScroll={handleScroll}
+        ref={tabsRef}
+      >
+        <h2
+          id={IngredientType.BUN}
+          className="ing-type-header text text_type_main-medium mb-6"
+        >
           Булки
         </h2>
         <ul className={styles.ingredientsList}>{buns.map(renderIngredient)}</ul>
         <h2
           id={IngredientType.SAUCE}
-          className="text text_type_main-medium mb-6"
+          className="ing-type-header text text_type_main-medium mb-6"
         >
           Соусы
         </h2>
         <ul className={styles.ingredientsList}>
           {sauces.map(renderIngredient)}
         </ul>
-        <h2 className="text text_type_main-medium mb-6">Начинки</h2>
-        <ul id={IngredientType.MAIN} className={styles.ingredientsList}>
+        <h2
+          id={IngredientType.MAIN}
+          className="ing-type-header text text_type_main-medium mb-6"
+        >
+          Начинки
+        </h2>
+        <ul className={styles.ingredientsList}>
           {mains.map(renderIngredient)}
         </ul>
       </div>
