f27fcdf57df6f31411ed94dfa58a71bac1ad389b
Artem Cherenkov <art.cherenkov@gmail.com>
2021-10-26 10:12:37 +0700

    sprint-3/step-1 refactor: починить стили хэдера

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -10,21 +10,25 @@ import styles from "./app-header.module.css";
 
 const AppHeader = () => (
   <header className={cn(styles.root)}>
-    <Link className={styles.logo} to="/">
-      <Logo />
-    </Link>
-    <Link className={cn(styles.link, styles.link_active)} to="/">
-      <BurgerIcon type="primary" />
-      <p className="text text_type_main-default ml-2">Конструктор</p>
-    </Link>
-    <Link className={styles.link} to="/feed">
-      <ListIcon type="secondary" />
-      <p className="text text_type_main-default ml-2">Лента заказов</p>
-    </Link>
-    <Link className={cn(styles.link, styles.link_right)} to="/profile">
-      <ProfileIcon type="secondary" />
-      <p className="text text_type_main-default ml-2">Личный кабинет</p>
-    </Link>
+    <div className={styles.container}>
+      <div className={styles.linksContainer}>
+        <Link className={styles.logo} to="/">
+          <Logo />
+        </Link>
+        <Link className={cn(styles.link, styles.link_active)} to="/">
+          <BurgerIcon type="primary" />
+          <p className="text text_type_main-default ml-2">Конструктор</p>
+        </Link>
+        <Link className={styles.link} to="/feed">
+          <ListIcon type="secondary" />
+          <p className="text text_type_main-default ml-2">Лента заказов</p>
+        </Link>
+        <Link className={cn(styles.link, styles.link_right)} to="/profile">
+          <ProfileIcon type="secondary" />
+          <p className="text text_type_main-default ml-2">Личный кабинет</p>
+        </Link>
+      </div>
+    </div>
   </header>
 );
 
