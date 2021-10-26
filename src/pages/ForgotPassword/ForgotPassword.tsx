511797a3a2e6d433a0f9e003e9aa370cfe79dd09
Artem Cherenkov <art.cherenkov@gmail.com>
2021-10-26 13:43:48 +0700

    sprint-3/step-1 feat: добавить верстку страниц восстановления и сброса пароля

--- src/pages/ForgotPassword/ForgotPassword.tsx
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,7 +1,39 @@
+import { Input } from "@ya.praktikum/react-developer-burger-ui-components";
 import React from "react";
+import AppHeader from "../../components/app-header/app-header";
+import Form, {
+  Hint,
+  InputContainer,
+  Submit,
+  Title,
+} from "../../components/form";
 
 const ForgotPassword: React.FC = () => {
-  return <div>ForgotPassword</div>;
+  const [value, setValue] = React.useState("");
+
+  return (
+    <>
+      <AppHeader />
+      <Form>
+        <Title>Восстановление пароля</Title>
+        <InputContainer>
+          <Input
+            name="email"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            type="text"
+            placeholder="Укажите e-mail"
+            error={false}
+            errorText="Ошибка"
+          />
+        </InputContainer>
+        <Submit>Восстановить</Submit>
+        <Hint linkHref="/login" linkTitle="Войти">
+          Вспомнили пароль?
+        </Hint>
+      </Form>
+    </>
+  );
 };
 
 export default ForgotPassword;
--- src/pages/ResetPassword/ResetPassword.tsx
+++ src/pages/ResetPassword/ResetPassword.tsx
@@ -1,7 +1,49 @@
+import {
+  Input,
+  PasswordInput,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 import React from "react";
+import AppHeader from "../../components/app-header/app-header";
+import Form, {
+  Hint,
+  InputContainer,
+  Submit,
+  Title,
+} from "../../components/form";
 
 const ResetPassword: React.FC = () => {
-  return <div>ResetPassword</div>;
+  const [value, setValue] = React.useState("");
+
+  return (
+    <>
+      <AppHeader />
+      <Form>
+        <Title>Восстановление пароля</Title>
+        <InputContainer>
+          <PasswordInput
+            name="password"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </InputContainer>
+        <InputContainer>
+          <Input
+            name="code"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            type="text"
+            placeholder="Введите код из письма"
+            error={false}
+            errorText="Ошибка"
+          />
+        </InputContainer>
+        <Submit>Сохранить</Submit>
+        <Hint linkHref="/login" linkTitle="Войти">
+          Вспомнили пароль?
+        </Hint>
+      </Form>
+    </>
+  );
 };
 
 export default ResetPassword;
