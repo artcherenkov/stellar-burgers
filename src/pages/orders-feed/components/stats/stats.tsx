04e0ce58424103d756ec10b150248216ca0c0827
Artem Cherenkov <art.cherenkov@gmail.com>
2021-12-26 18:50:08 +0700

    sprint-5/step-1 refactor: добавить верстку статистики заказов

--- src/pages/orders-feed/components/stats/stats.tsx
+++ src/pages/orders-feed/components/stats/stats.tsx
@@ -1,7 +1,89 @@
-import styles from "../../orders-feed.module.css";
+import cn from "classnames";
+import styles from "./stats.module.css";
 
 const Stats = () => {
-  return <div className={styles.stats}/>;
+  return (
+    <div className={styles.stats}>
+      <div className={styles.ordersStatuses}>
+        <div className={styles.ordersStatus}>
+          <p className="text text_type_main-medium mb-6">Готовы:</p>
+          <ul className={styles.idsList}>
+            <li
+              className={cn(
+                styles.orderId,
+                styles.orderId_highlight,
+                "text text_type_digits-default"
+              )}
+            >
+              034533
+            </li>
+            <li
+              className={cn(
+                styles.orderId,
+                styles.orderId_highlight,
+                "text text_type_digits-default"
+              )}
+            >
+              034533
+            </li>
+            <li
+              className={cn(
+                styles.orderId,
+                styles.orderId_highlight,
+                "text text_type_digits-default"
+              )}
+            >
+              034533
+            </li>
+            <li
+              className={cn(
+                styles.orderId,
+                styles.orderId_highlight,
+                "text text_type_digits-default"
+              )}
+            >
+              034533
+            </li>
+          </ul>
+        </div>
+        <div className={styles.ordersStatus}>
+          <p className="text text_type_main-medium mb-6">В работе:</p>
+          <ul className={styles.idsList}>
+            <li className={cn(styles.orderId, "text text_type_digits-default")}>
+              034538
+            </li>
+            <li className={cn(styles.orderId, "text text_type_digits-default")}>
+              034538
+            </li>
+            <li className={cn(styles.orderId, "text text_type_digits-default")}>
+              034538
+            </li>
+            <li className={cn(styles.orderId, "text text_type_digits-default")}>
+              034538
+            </li>
+            <li className={cn(styles.orderId, "text text_type_digits-default")}>
+              034538
+            </li>
+            <li className={cn(styles.orderId, "text text_type_digits-default")}>
+              034538
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.globalStats}>
+        <p className="text text_type_main-medium">Выполнено за все время:</p>
+        <span className={cn(styles.stat, "text text_type_digits-large")}>
+          28 752
+        </span>
+      </div>
+      <div className={styles.globalStats}>
+        <p className="text text_type_main-medium">Выполнено за сегодня:</p>
+        <span className={cn(styles.stat, "text text_type_digits-large")}>
+          138
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Stats;
