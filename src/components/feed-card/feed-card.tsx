f805b9e3188cb7a15d4f1828b12ca7785d902a4e
Artem Cherenkov <art.cherenkov@gmail.com>
2022-01-09 20:08:42 +0700

    sprint-5/step-1 refactor: доработать роутинг

--- src/components/feed-card/feed-card.tsx
+++ src/components/feed-card/feed-card.tsx
@@ -1,34 +1,46 @@
 import cn from "classnames";
+import { Link, useLocation } from "react-router-dom";
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import IngredientImage from "./components/ingredient-image/ingredient-image";
 import styles from "./feed-card.module.css";
+import * as H from "history";
 
 const FeedCard = () => {
+  const location = useLocation<{ background?: H.Location }>();
+
   return (
-    <li className={styles.card}>
-      <p className={cn(styles.header, "text text_type_digits-default")}>
-        #034535{" "}
-        <time className="text text_type_main-default text_color_inactive">
-          Сегодня, 16:20 i-GMT+3
-        </time>
-      </p>
-      <h2 className={cn(styles.title, "text text_type_main-medium")}>
-        Death Star Starship Main бургер
-      </h2>
-      <div className={styles.priceInfo}>
-        <ul className={styles.ingredientsList}>
-          <IngredientImage src="https://code.s3.yandex.net/react/code/bun-02-mobile.png" />
-          <IngredientImage src="https://code.s3.yandex.net/react/code/bun-02-mobile.png" />
-          <IngredientImage src="https://code.s3.yandex.net/react/code/bun-02-mobile.png" />
-          <IngredientImage src="https://code.s3.yandex.net/react/code/bun-02-mobile.png" />
-        </ul>
-        <p className={cn(styles.priceContainer)}>
-          <span className={cn(styles.price, "text text_type_digits-default")}>
-            480
-          </span>
-          <CurrencyIcon type="primary" />
+    <li className={styles.cardContainer}>
+      <Link
+        to={{
+          pathname: `feed/1`,
+          state: { background: location },
+        }}
+        className={styles.card}
+      >
+        <p className={cn(styles.header, "text text_type_digits-default")}>
+          #034535{" "}
+          <time className="text text_type_main-default text_color_inactive">
+            Сегодня, 16:20 i-GMT+3
+          </time>
         </p>
-      </div>
+        <h2 className={cn(styles.title, "text text_type_main-medium")}>
+          Death Star Starship Main бургер
+        </h2>
+        <div className={styles.priceInfo}>
+          <ul className={styles.ingredientsList}>
+            <IngredientImage src="https://code.s3.yandex.net/react/code/bun-02-mobile.png" />
+            <IngredientImage src="https://code.s3.yandex.net/react/code/bun-02-mobile.png" />
+            <IngredientImage src="https://code.s3.yandex.net/react/code/bun-02-mobile.png" />
+            <IngredientImage src="https://code.s3.yandex.net/react/code/bun-02-mobile.png" />
+          </ul>
+          <p className={cn(styles.priceContainer)}>
+            <span className={cn(styles.price, "text text_type_digits-default")}>
+              480
+            </span>
+            <CurrencyIcon type="primary" />
+          </p>
+        </div>
+      </Link>
     </li>
   );
 };
--- src/pages/order-details/order-details.tsx
+++ src/pages/order-details/order-details.tsx
@@ -1,208 +1,206 @@
-import Layout from "../../components/layout/layout";
+import React from "react";
+import cn from "classnames";
 import styles from "./order-details.module.css";
 import IngredientImage from "../../components/feed-card/components/ingredient-image/ingredient-image";
-import cn from "classnames";
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
-const OrderDetails = () => {
+const OrderDetails = ({ style }: { style?: React.CSSProperties }) => {
   return (
-    <Layout>
-      <div className={styles.root}>
-        <p
-          className="text text_type_digits-default mb-10"
-          style={{ textAlign: "center" }}
-        >
-          #034533
-        </p>
-        <h1 className="text text_type_main-medium mb-3">
-          Black Hole Singularity острый бургер
-        </h1>
-        <p className="text text_type_main-default mb-15">Выполнен</p>
-        <p className="text text_type_main-medium mb-6">Состав:</p>
-        <ul className={cn(styles.list, "custom-scroll mb-10")}>
-          <li className={styles.ingredient}>
-            <IngredientImage
-              style={{ margin: 0 }}
-              src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
-              renderDiv
-            />
-            <p
-              className={cn(
-                styles.title,
-                "text text_type_main-default ml-4 mr-4"
-              )}
-            >
-              Флюоресцентная булка R2-D3
-            </p>
-            <p className={styles.priceContainer}>
-              <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
-              <CurrencyIcon type="primary" />
-            </p>
-          </li>
-          <li className={styles.ingredient}>
-            <IngredientImage
-              style={{ margin: 0 }}
-              src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
-              renderDiv
-            />
-            <p
-              className={cn(
-                styles.title,
-                "text text_type_main-default ml-4 mr-4"
-              )}
-            >
-              Флюоресцентная булка R2-D3
-            </p>
-            <p className={styles.priceContainer}>
-              <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
-              <CurrencyIcon type="primary" />
-            </p>
-          </li>
-          <li className={styles.ingredient}>
-            <IngredientImage
-              style={{ margin: 0 }}
-              src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
-              renderDiv
-            />
-            <p
-              className={cn(
-                styles.title,
-                "text text_type_main-default ml-4 mr-4"
-              )}
-            >
-              Флюоресцентная булка R2-D3
-            </p>
-            <p className={styles.priceContainer}>
-              <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
-              <CurrencyIcon type="primary" />
-            </p>
-          </li>
-          <li className={styles.ingredient}>
-            <IngredientImage
-              style={{ margin: 0 }}
-              src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
-              renderDiv
-            />
-            <p
-              className={cn(
-                styles.title,
-                "text text_type_main-default ml-4 mr-4"
-              )}
-            >
-              Флюоресцентная булка R2-D3
-            </p>
-            <p className={styles.priceContainer}>
-              <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
-              <CurrencyIcon type="primary" />
-            </p>
-          </li>
-          <li className={styles.ingredient}>
-            <IngredientImage
-              style={{ margin: 0 }}
-              src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
-              renderDiv
-            />
-            <p
-              className={cn(
-                styles.title,
-                "text text_type_main-default ml-4 mr-4"
-              )}
-            >
-              Флюоресцентная булка R2-D3
-            </p>
-            <p className={styles.priceContainer}>
-              <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
-              <CurrencyIcon type="primary" />
-            </p>
-          </li>
-          <li className={styles.ingredient}>
-            <IngredientImage
-              style={{ margin: 0 }}
-              src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
-              renderDiv
-            />
-            <p
-              className={cn(
-                styles.title,
-                "text text_type_main-default ml-4 mr-4"
-              )}
-            >
-              Флюоресцентная булка R2-D3
-            </p>
-            <p className={styles.priceContainer}>
-              <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
-              <CurrencyIcon type="primary" />
-            </p>
-          </li>
-          <li className={styles.ingredient}>
-            <IngredientImage
-              style={{ margin: 0 }}
-              src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
-              renderDiv
-            />
-            <p
-              className={cn(
-                styles.title,
-                "text text_type_main-default ml-4 mr-4"
-              )}
-            >
-              Флюоресцентная булка R2-D3
-            </p>
-            <p className={styles.priceContainer}>
-              <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
-              <CurrencyIcon type="primary" />
-            </p>
-          </li>
-          <li className={styles.ingredient}>
-            <IngredientImage
-              style={{ margin: 0 }}
-              src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
-              renderDiv
-            />
-            <p
-              className={cn(
-                styles.title,
-                "text text_type_main-default ml-4 mr-4"
-              )}
-            >
-              Флюоресцентная булка R2-D3
-            </p>
-            <p className={styles.priceContainer}>
-              <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
-              <CurrencyIcon type="primary" />
-            </p>
-          </li>
-          <li className={styles.ingredient}>
-            <IngredientImage
-              style={{ margin: 0 }}
-              src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
-              renderDiv
-            />
-            <p
-              className={cn(
-                styles.title,
-                "text text_type_main-default ml-4 mr-4"
-              )}
-            >
-              Флюоресцентная булка R2-D3
-            </p>
-            <p className={styles.priceContainer}>
-              <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
-              <CurrencyIcon type="primary" />
-            </p>
-          </li>
-        </ul>
-        <p className={styles.info}>
-          <time className="text text_type_main-default text_color_inactive">
-            Вчера, 13:50 i-GMT+3
-          </time>
-          <p style={{ marginLeft: "auto" }} className={styles.priceContainer}>
+    <div style={style} className={styles.root}>
+      <p
+        className="text text_type_digits-default mb-10"
+        style={{ textAlign: "center" }}
+      >
+        #034533
+      </p>
+      <h1 className="text text_type_main-medium mb-3">
+        Black Hole Singularity острый бургер
+      </h1>
+      <p className="text text_type_main-default mb-15">Выполнен</p>
+      <p className="text text_type_main-medium mb-6">Состав:</p>
+      <ul className={cn(styles.list, "custom-scroll mb-10")}>
+        <li className={styles.ingredient}>
+          <IngredientImage
+            style={{ margin: 0 }}
+            src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+            renderDiv
+          />
+          <p
+            className={cn(
+              styles.title,
+              "text text_type_main-default ml-4 mr-4"
+            )}
+          >
+            Флюоресцентная булка R2-D3
+          </p>
+          <p className={styles.priceContainer}>
             <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
             <CurrencyIcon type="primary" />
           </p>
+        </li>
+        <li className={styles.ingredient}>
+          <IngredientImage
+            style={{ margin: 0 }}
+            src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+            renderDiv
+          />
+          <p
+            className={cn(
+              styles.title,
+              "text text_type_main-default ml-4 mr-4"
+            )}
+          >
+            Флюоресцентная булка R2-D3
+          </p>
+          <p className={styles.priceContainer}>
+            <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
+            <CurrencyIcon type="primary" />
+          </p>
+        </li>
+        <li className={styles.ingredient}>
+          <IngredientImage
+            style={{ margin: 0 }}
+            src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+            renderDiv
+          />
+          <p
+            className={cn(
+              styles.title,
+              "text text_type_main-default ml-4 mr-4"
+            )}
+          >
+            Флюоресцентная булка R2-D3
+          </p>
+          <p className={styles.priceContainer}>
+            <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
+            <CurrencyIcon type="primary" />
+          </p>
+        </li>
+        <li className={styles.ingredient}>
+          <IngredientImage
+            style={{ margin: 0 }}
+            src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+            renderDiv
+          />
+          <p
+            className={cn(
+              styles.title,
+              "text text_type_main-default ml-4 mr-4"
+            )}
+          >
+            Флюоресцентная булка R2-D3
+          </p>
+          <p className={styles.priceContainer}>
+            <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
+            <CurrencyIcon type="primary" />
+          </p>
+        </li>
+        <li className={styles.ingredient}>
+          <IngredientImage
+            style={{ margin: 0 }}
+            src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+            renderDiv
+          />
+          <p
+            className={cn(
+              styles.title,
+              "text text_type_main-default ml-4 mr-4"
+            )}
+          >
+            Флюоресцентная булка R2-D3
+          </p>
+          <p className={styles.priceContainer}>
+            <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
+            <CurrencyIcon type="primary" />
+          </p>
+        </li>
+        <li className={styles.ingredient}>
+          <IngredientImage
+            style={{ margin: 0 }}
+            src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+            renderDiv
+          />
+          <p
+            className={cn(
+              styles.title,
+              "text text_type_main-default ml-4 mr-4"
+            )}
+          >
+            Флюоресцентная булка R2-D3
+          </p>
+          <p className={styles.priceContainer}>
+            <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
+            <CurrencyIcon type="primary" />
+          </p>
+        </li>
+        <li className={styles.ingredient}>
+          <IngredientImage
+            style={{ margin: 0 }}
+            src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+            renderDiv
+          />
+          <p
+            className={cn(
+              styles.title,
+              "text text_type_main-default ml-4 mr-4"
+            )}
+          >
+            Флюоресцентная булка R2-D3
+          </p>
+          <p className={styles.priceContainer}>
+            <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
+            <CurrencyIcon type="primary" />
+          </p>
+        </li>
+        <li className={styles.ingredient}>
+          <IngredientImage
+            style={{ margin: 0 }}
+            src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+            renderDiv
+          />
+          <p
+            className={cn(
+              styles.title,
+              "text text_type_main-default ml-4 mr-4"
+            )}
+          >
+            Флюоресцентная булка R2-D3
+          </p>
+          <p className={styles.priceContainer}>
+            <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
+            <CurrencyIcon type="primary" />
+          </p>
+        </li>
+        <li className={styles.ingredient}>
+          <IngredientImage
+            style={{ margin: 0 }}
+            src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+            renderDiv
+          />
+          <p
+            className={cn(
+              styles.title,
+              "text text_type_main-default ml-4 mr-4"
+            )}
+          >
+            Флюоресцентная булка R2-D3
+          </p>
+          <p className={styles.priceContainer}>
+            <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
+            <CurrencyIcon type="primary" />
+          </p>
+        </li>
+      </ul>
+      <p className={styles.info}>
+        <time className="text text_type_main-default text_color_inactive">
+          Вчера, 13:50 i-GMT+3
+        </time>
+        <p style={{ marginLeft: "auto" }} className={styles.priceContainer}>
+          <span className="text text_type_main-default mr-2">2 x 20</span>{" "}
+          <CurrencyIcon type="primary" />
         </p>
-      </div>
-    </Layout>
+      </p>
+    </div>
   );
 };
 
